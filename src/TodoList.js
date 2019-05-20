75052fe62608c96e1915127a863a1ce5e2125818
Hasier Pastor <hasier.pastor@gmail.com>
2019-05-20 16:09:57 -0700

    made todolist a functional component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,35 +1,13 @@
 import React, { Component } from 'react';
 import Todo from './Todo';
 
-//TODO: Add form, add uuid
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [
-        { name: 'Groceries', id: 1 },
-        { name: 'Study', id: 2 },
-        { name: 'Sleep', id: 3 },
-        { name: 'Party', id: 4 }
-      ]
-    };
-  }
+const TodoList = props => {
+  let { todos, removeTodo } = props;
+  const list = todos.map(todo => {
+    return <Todo name={todo.name} id={todo.id} removeTodo={removeTodo} />;
+  });
 
-  removeTodo = name => {
-    let filteredTodos = this.state.todos.filter(todo => {
-      return todo.name !== name;
-    });
-    this.setState({ todos: filteredTodos });
-  };
-
-  render() {
-    const list = this.state.todos.map(todo => {
-      return (
-        <Todo name={todo.name} id={todo.id} removeTodo={this.removeTodo} />
-      );
-    });
-    return <ul>{list}</ul>;
-  }
-}
+  return <ul>{list}</ul>;
+};
 
 export default TodoList;
