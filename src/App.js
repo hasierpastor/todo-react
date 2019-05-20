c4eae48e45c0968dc8bbad4bc91a8180149854ca
Hasier Pastor <hasier.pastor@gmail.com>
2019-05-20 16:06:58 -0700

    created a form to add todo, lifted state to app component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import './App.css';
 import TodoList from './TodoList';
 
 class App extends Component {
+  constructor(props) {
+    this.state = {
+      todos: [],
+      todo: ''
+    };
+  }
+
+  addTodo = todo => {};
+
+  removeTodo = name => {
+    let filteredTodos = this.state.todos.filter(todo => {
+      return todo.name !== name;
+    });
+    this.setState({ todos: filteredTodos });
+  };
+
   render() {
-    return <TodoList />;
+    return <TodoList removeTodo={removeTodo} addTodo={addTodo} />;
   }
 }
 
